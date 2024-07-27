a01b1ab0cb121d4c6af5665205e05de8978e6f53
Shyam Nair <110102576+Shyamnair488@users.noreply.github.com>
2024-07-27 22:31:36 +0530

    changes

---
+++ Day 16/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ==================*/
+var typed = new Typed(".typing",{
+    strings: [" ", "Machine Learning Specialist", "Python Developer", "SQL Specialist", "C++ Programmer", "IoT Developer", "Robotics Engineer", "Teacher", "Web Designer", "Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
